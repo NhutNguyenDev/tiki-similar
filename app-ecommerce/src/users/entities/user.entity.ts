362005738826400b328c50087a2cc9423f60ef27
nttlinh2912 <nthithuylinh93@gmail.com>
2025-02-07 09:18:56 +0700

    typeorm

---
+++ app-ecommerce/src/users/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRole } from '../enums/user-role.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.BUYER })
+  role: UserRole;
+}
