0f09a17d1b3d1c4ac2ad1ad838dc884450937ef2
nttlinh2912 <nthithuylinh93@gmail.com>
2025-02-09 09:26:29 +0700

    run

--- app-ecommerce/src/app.module.ts
+++ app-ecommerce/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {User} from './users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456Linh',
+      database: 'tiki_similar',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
